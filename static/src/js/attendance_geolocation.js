abe09f85c63b1229ad8a525faffb588e6d9c2501
Yudha Pradipta Ramadan <yudha.pradipta@ecomindo.com>
2021-06-13 13:59:08 +0700

    fix js

--- static/src/js/attendance_geolocation.js
+++ static/src/js/attendance_geolocation.js
@@ -1,42 +1,35 @@
 odoo.define('hr_attendance_geolocation.attendances_geolocation', function (require) {
     "use strict";
 
-    var MyAttendances = require('hr_attendance.my_attendances');
+    const MyAttendances = require('hr_attendance.my_attendances');
 
     MyAttendances.include({
-        init: function (parent, action) {
-            this._super.apply(this, arguments);
-            this.location = (null, null);
-            this.errorCode = null;
-        },
         update_attendance: function () {
-            var self = this;
-            var options = {
+            const options = {
                 enableHighAccuracy: true,
                 timeout: 5000,
                 maximumAge: 0
             };
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(self._manual_attendance.bind(self), self._getPositionError, options);
+                navigator.geolocation.getCurrentPosition(this._manual_attendance.bind(this), this._getPositionError, options);
             }
         },
-        _manual_attendance: function (position) {
-            var self = this;
+        _check_attendance: function (position) {
             this._rpc({
                 model: 'hr.employee',
-                method: 'attendance_manual',
-                args: [[self.employee.id], 'hr_attendance.hr_attendance_action_my_attendances', null, [position.coords.latitude, position.coords.longitude]],
+                method: 'check_attendance',
+                args: [[this.employee.id], 'hr_attendance.hr_attendance_action_my_attendances', null, [position.coords.latitude, position.coords.longitude]],
             })
             .then(function(result) {
                 if (result.action) {
-                    self.do_action(result.action);
+                    this.do_action(result.action);
                 } else if (result.warning) {
-                    self.do_warn(result.warning);
+                    this.do_warn(result.warning);
                 }
             });
         },
         _getPositionError: function (error) {
-            console.warn('ERROR(${error.code}): ${error.message}');
+            console.warn("You need allow this site to access your location to check-in or check-out");
         },
     });
 
